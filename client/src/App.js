b0acecea36e74603bb726a3c32c2b92ae585fda3
Kamilla <kamilla.sthefany@outlook.com>
2023-09-22 20:06:47 -0300

    Projeto front em branco

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Projeto em branco
+        </p>        
+      </header>
+    </div>
+  );
+}
+
+export default App;
